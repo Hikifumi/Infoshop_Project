bb3076014a16d5f544e95f85a7473aed04f16b10
Hikifumi <fm9lewandowski@gmail.com>
2022-07-09 14:54:07 +0700

    Save Berhasil

--- src/infoshop_project/FXML3EditController.java
+++ src/infoshop_project/FXML3EditController.java
@@ -1,9 +1,17 @@
 package infoshop_project;
 
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import static javafx.collections.FXCollections.observableArrayList;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,8 +27,11 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 public class FXML3EditController implements Initializable {
+    XStream xstream = new XStream(new StaxDriver());
     ItemList data;
-    ArrayList<String> dataList = new ArrayList<>();
+    ArrayList<Item> ListItem = new ArrayList<Item>();
+    ObservableList<Item> observableListItem = observableArrayList();
+//    ArrayList<Item> ListItem = new ArrayList<Item>();
     
     @FXML
     private Label lSeller;
@@ -91,22 +102,98 @@ public class FXML3EditController implements Initializable {
         tvMerch.setItems(data.getData());
         System.out.println("Barang telah ditambahkan");
         
-//        if (tfIndex.getText().equals("")) {
-//            dataList.add(tfName.getText());
-//            dataList.add(tfStock.getText());
-//            dataList.add(tfType.getText());
-//            dataList.add(tfPrice.getText());
-//        } else dataList.add(Integer.valueOf(tfIndex.getText(), tfName.getText(), tfStock.getText(), tfType.getText(), tfPrice.getText()));
-//        
         tfName.setText("");
         tfStock.setText("");
         tfType.setText("");
         tfPrice.setText("");
+        
+        Item dataBarang = new Item(name, stock2, type, price2);
+        ListItem.add(dataBarang);
     }
     
     public void getData(String d){
         lSeller.setText("Hi, " + d + "! Sehat po?");
         System.out.println("getData method is Invoked");
+    }
+    
+    @FXML
+    private void saveButtonAction(ActionEvent event) {
+        File f = new File("dataBarang.xml");
+        if (f.exists() && !f.isDirectory()) {
+            System.out.println("file nya adaaa");
+            OpenXml();
+        }
+
+        SaveAndCreate();
+        System.out.println("Bismillah Save");
+    }
+    
+    @FXML
+    private void deleteButtonAction(ActionEvent event) {
+        int selected = tvMerch.getSelectionModel().getSelectedIndex();
+        tvMerch.getItems().remove(selected);
+    }
+    
+    @FXML
+    private void editButtonAction(ActionEvent event) {
+        
+    }
+    
+    void OpenXml() {
+        FileInputStream inputDoc;
+
+        try {
+            inputDoc = new FileInputStream("dataBarang.xml");
+            int content;
+            char c;
+            String s = "";
+            while ((content = inputDoc.read()) != -1) {
+                c = (char) content;
+                s += c;
+            }
+
+            ListItem = (ArrayList<Item>) xstream.fromXML(s);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    void SaveAndCreate() {
+        FileOutputStream outputDoc;
+        String xml = xstream.toXML(ListItem);
+        File f = new File("dataBarang.xml");
+        try {
+            byte[] data = xml.getBytes();
+            outputDoc = new FileOutputStream("dataBarang.xml");
+            outputDoc.write(data);
+            outputDoc.close();
+            System.out.println("add data success");
+        } catch (Exception error) {
+            System.err.println("An error occur: " + error.getMessage());
+        }
+    }
+    
+    void OpenData() {
+        XStream xStream = new XStream(new StaxDriver());
+        FileInputStream berkasMasuk;
+
+        try {
+            berkasMasuk = new FileInputStream("dataBarang.xml");
+            int isi;
+            char c;
+            String s = "";
+            while ((isi = berkasMasuk.read()) != -1) {
+
+                c = (char) isi;
+                s = s + c;
+
+            }
+
+            ListItem = (ArrayList<Item>) xstream.fromXML(s);
+            berkasMasuk.close();
+        } catch (Exception e) {
+            System.out.println("terjadi kesalahan");
+        }
     }
     
     @Override
@@ -117,8 +204,19 @@ public class FXML3EditController implements Initializable {
         tcPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
         
         data = new ItemList();
-        data.setDummy();
+//        data.setDummy();
         tvMerch.setItems(data.getData());
+        
+        File f = new File("dataBarang.xml");
+        if (f.exists() && !f.isDirectory()) {
+            OpenData();
+            for (int i = 0; i < ListItem.size(); i++) {
+                observableListItem.add(ListItem.get(i));
+            }
+
+        }
+
+        tvMerch.setItems(observableListItem);
     }    
     
 }
